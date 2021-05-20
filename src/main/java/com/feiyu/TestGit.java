0014b38703915582df829359fb4abe8074e1e340
feiyu <feiyu@outllok.com>
2021-05-20 16:57:43 +0800

    first commit

---
+++ src/main/java/com/feiyu/TestGit.java
@@ -0,0 +1,12 @@
+package com.feiyu;
+
+/**
+ * @author feiye
+ * @date 2021/5/20 -
+ **/
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
